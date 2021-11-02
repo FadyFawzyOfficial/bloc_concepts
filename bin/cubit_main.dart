f08e97838b2038fb3672ca981b6f5113f54865e5
Fady Fawzy <FadyFawzy.Official@gmail.com>
2021-11-02 11:56:40 +0200

    Listen to the Cubit Stream (& Change States according to)

--- bin/cubit_main.dart
+++ bin/cubit_main.dart
@@ -1,35 +1,37 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 
-// Here we're declaring the Cubit for managing the counter feature.
+// Here we're declaring the Cubit responsible for managing the counter feature.
 class CounterCubit extends Cubit<int> {
-  // We are set the default 'state' of the counter to zero in the constractor.
+  // We are set the default 'state' of the counter to zero in the constructor.
   CounterCubit() : super(0);
 
-  // Then we create an increment & decrement functions so that when we call it
-  // from the UI, then your increment & decrement in state will be emitted
-  void increment() => emit(state + 1);
+  // Then we create an increament & decrement functions so that when we call it
+  // from the UI, then you increament & decreament in state will be emitted.
+  void increament() => emit(state + 1);
   void decrement() => emit(state - 1);
 }
 
 // The main function acts like the UI currently.
 // The function is designed for eductional purpose to easy understand.
-void main(List<String> args) {
-  // We declate the CounterCubit.
+Future<void> main(List<String> arguments) async {
+  // Cubit is actually a type of Stream emitting states.
   final cubit = CounterCubit();
 
-  // Then we can observe how we can update the state of the counter value easily
-  // by calling the specific function we created earlier (inside cubit)
-  print(cubit.state); //! Should print 0 = the initial state of the counter
+  // You can listen to it and observe the state change
+  //! this subscribtion to the cubit state stream and prints the state value
+  //! emitted by it.
+  final streamSubscription = cubit.listen(print);
 
-  cubit.increment();
+  cubit.increament();
+  cubit.increament();
+  cubit.increament();
+  cubit.increament();
 
-  print(cubit.state); //! Should print 1
+  //! We use this to not cancel the subscribtion immediately down here
+  await Future.delayed(Duration.zero);
 
-  cubit.increment();
-
-  print(cubit.state); //! Should print 2
-
-  cubit.decrement();
-
-  print(cubit.state); //! Should print 1
+  await streamSubscription.cancel();
+  await cubit.close();
 }
